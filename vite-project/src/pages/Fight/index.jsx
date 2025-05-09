e77840dd992f44c4e2bca9181116fe135dc3757c
thongprotk <buitrungt242@gmail.com>
2025-05-09 17:45:54 +0700

    train

--- vite-project/src/pages/Fight/index.jsx
+++ vite-project/src/pages/Fight/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import FightContent from "./FightContent";
 import Header from "../../component/header/headerFight";
-import Header from "../../component/header/headerFight";
 // import ManVsMan from "../../component/footer/manVsMan";
 import ModalInformationWin from "./modaIWin";
 import ModalInformationLose from "./modalLose";
@@ -40,10 +39,7 @@ export default function Fight() {
   const handleRestart = () => {
     socket.emit("playerClicked", {
       roomID,
-      roomID,
     });
-  };
-  useEffect(() => {
   };
   useEffect(() => {
     socket.on("playAgain", (data) => {
@@ -66,11 +62,6 @@ export default function Fight() {
   //   socket.on("player-left", handlePlayerLeft);
   // };
 
-    return () => socket.off("playAgain");
-  }, []);
-  const exitGame = () => {
-    socket.emit("exitGame", { roomID: roomID });
-  };
   const checkGame = () => {
     let countManOption = manOption.filter((num) => num === 1).length;
     let countOpponentOption = opponentOption.filter((num) => num === 1).length;
@@ -87,7 +78,6 @@ export default function Fight() {
   }, [manOption, opponentOption]);
   useEffect(() => {
     if (!saveResult) return;
-    if (!saveResult) return;
     socket.emit("resultGame", {
       roomID,
       result,
@@ -97,11 +87,9 @@ export default function Fight() {
       const timeOut = setTimeout(() => {
         setShowModal(true);
       }, 1100);
-      }, 1100);
       return () => clearTimeout(timeOut);
     }
     return () => {
-      socket.off("resultGame");
       socket.off("resultGame");
     };
   }, [saveResult]);
